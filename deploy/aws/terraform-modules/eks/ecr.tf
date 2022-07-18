263e2fa14529d67bf1d17cfade00185cf3bb207c
Tiago Mello <tiago@rubarb.app>
2022-07-18 16:23:45 +0200

    Expire untagged images after 7 days

--- deploy/aws/terraform-modules/eks/ecr.tf
+++ deploy/aws/terraform-modules/eks/ecr.tf
@@ -15,3 +15,33 @@ resource "aws_ecr_repository" "retriever" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "expire_untagged_images" {
+  for_each = toset(["uploader", "retriever"])
+
+  repository = each.value
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 7 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 7
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+
+  depends_on = [
+    "aws_ecr_repository.uploader",
+    "aws_ecr_repository.retriever",
+  ]
+}
