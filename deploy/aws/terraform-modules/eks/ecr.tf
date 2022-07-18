1eb649c2c140fc31097526c2a362f9fb8cd526dc
Tiago Mello <tiago@rubarb.app>
2022-07-18 15:49:14 +0200

    Fix

--- deploy/aws/terraform-modules/eks/ecr.tf
+++ deploy/aws/terraform-modules/eks/ecr.tf
@@ -1,5 +1,14 @@
-resource "aws_ecr_repository" "mydds" {
-  name                 = "mydds"
+resource "aws_ecr_repository" "uploader" {
+  name                 = "uploader"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "retriever" {
+  name                 = "retriever"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
